cd0f47ce3b231e193c4de836627e27946e66975c
Chizoba Victory <chizoba.victory1@gmail.com>
2022-12-05 20:20:01 +0100

    Added dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import React from 'react';
+import Register from "./pages/Register/Register"
+import Home from './pages/Home/Home';
+import Login from "./pages/Login/Login"
+import Restaurant from './pages/Restaurant/Restaurant';
+import OTP from './pages/OTP/OTP';
+import DataProvider from "./context/authcontext";
+import {ToastContainer} from 'react-toastify'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+       <DataProvider>
+        <ToastContainer />
+        <Router>
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/restaurant" element={<Restaurant />} />
+            <Route path="/otp" element={<OTP />} />
+            {/* <Route path="/contact" element={<Contact />} /> */}
+          </Routes>
+      </Router>
+    </DataProvider>
+    </React.Fragment>
   );
 }
 
